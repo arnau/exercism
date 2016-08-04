61d953dcd100b4e4045cad2dcf12e20da9d56f5e
Arnau Siches <arnau@ustwo.com>
2016-08-04 06:26:38 +0100

    elm: Use uncons in RNA transcription

--- elm/rna-transcription/RNATranscription.elm
+++ elm/rna-transcription/RNATranscription.elm
@@ -5,17 +5,28 @@ import Result
 
 toRNA : String -> Result Char String
 toRNA dna =
-  String.toList dna
-    |> List.map transcript
-    |> List.foldr (\x rna -> Result.map2 String.cons x rna) (Ok "")
+  case String.uncons dna of
+    Nothing ->
+      Ok ""
+
+    Just (nucleotide, chain) ->
+      Result.map2 String.cons (transcript nucleotide) (toRNA chain)
+
 
 transcript : Char -> Result Char Char
 transcript nucleotide =
   case nucleotide of
-    'G' -> Ok 'C'
-    'C' -> Ok 'G'
-    'T' -> Ok 'A'
-    'A' -> Ok 'U'
-    n   -> Err n
+    'G' ->
+      Ok 'C'
 
+    'C' ->
+      Ok 'G'
 
+    'T' ->
+      Ok 'A'
+
+    'A' ->
+      Ok 'U'
+
+    _ ->
+      Err nucleotide
