e17c5bba5a7c68ce964635467b44b7d6a0cdd397
Arnau Siches <asiches@gmail.com>
2016-08-11 06:58:39 +0100

    elm: Add List ops

---
+++ elm/list-ops/ListOps.elm
@@ -0,0 +1,54 @@
+module ListOps exposing (..)
+
+length : List a -> Int
+length xs =
+  foldr (\_ n -> n + 1) 0 xs
+
+
+reverse : List a -> List a
+reverse xs =
+  foldl (\x acc -> x :: acc) [] xs
+
+
+map : (a -> b) -> List a -> List b
+map fn xs =
+  foldr (\x acc -> fn x :: acc) [] xs
+
+
+filter : (a -> Bool) -> List a -> List a
+filter fn xs =
+  foldr (\x acc -> if fn x then x :: acc else acc) [] xs
+
+
+foldl : (a -> b -> b) -> b -> List a -> b
+foldl fn b xs =
+  case xs of
+    [] -> b
+    x :: xs' ->
+      foldl fn (fn x b) xs'
+
+
+foldl' : (a -> b -> b) -> b -> List a -> b
+foldl' fn b xs =
+  case xs of
+    [] -> b
+    x :: xs' ->
+      foldl' fn (fn x b) xs'
+
+
+foldr : (a -> b -> b) -> b -> List a -> b
+foldr fn b xs =
+  case xs of
+    [] -> b
+    x :: xs' ->
+      fn x (foldr fn b xs')
+
+
+append : List a -> List a -> List a
+append xs ys =
+  foldr (\x acc -> x :: acc) ys xs
+
+
+concat : List (List a) -> List a
+concat xs =
+  foldr append [] xs
